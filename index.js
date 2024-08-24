a4d0f9f739ed4c1b1eb904be31baec55d8550391
Aniket Dutta <114622119+anikkkettt@users.noreply.github.com>
2024-08-25 04:21:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket('ws://tcp-vs-udp-visualizer.vercel.app');
+const ws = new WebSocket('wss://tcp-vs-udp-visualizer.vercel.app');
 
 let tcpChart = new Chart(document.getElementById('tcpChart').getContext('2d'), {
   type: 'line',
