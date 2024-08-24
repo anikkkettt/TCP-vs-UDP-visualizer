9f9a9dea95ec09cc2a0ae03c618983641911465a
Aniket Dutta <114622119+anikkkettt@users.noreply.github.com>
2024-08-25 04:58:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -141,6 +141,6 @@ function simulateUDP(ws) {
   }
 }
 
-server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is listening on port ${server.address().port}`);
 });
